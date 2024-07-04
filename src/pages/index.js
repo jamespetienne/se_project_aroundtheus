882c26b5f142184d06a68e0f2015ea4389f55010
James Etienne <jamespetienne@gmail.com>
2024-07-04 12:48:24 -0400

    Project 8 OOP and Webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
+/** 
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
+import "../pages/index.css";
 
 // Initial Cards Data
 const initialCards = [
@@ -159,5 +161,83 @@ profileEditFormValidator.enableValidation();
 const addCardFormValidator = new FormValidator(
   validationSettings,
   addCardFormElement
+);
+addCardFormValidator.enableValidation();
+*/
+
+import Section from "../components/Section.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
+import {
+  initialCards,
+  validationSettings,
+  profileSelectors,
+} from "../utils/utils.js";
+import Card from "../components/Card.js";
+import FormValidator from "../components/FormValidator.js";
+import "../pages/index.css";
+
+// Initialize Cards Section
+const cardSection = new Section(
+  {
+    items: initialCards,
+    renderer: (cardData) => {
+      const card = new Card(cardData, "#card-template", handleImageClick);
+      const cardElement = card.getView();
+      cardSection.addItem(cardElement);
+    },
+  },
+  ".gallery__cards"
+);
+
+cardSection.renderItems();
+
+// Initialize Popups
+const imagePopup = new PopupWithImage("#preview-image-modal");
+imagePopup.setEventListeners();
+
+const addCardPopup = new PopupWithForm("#add-card-modal", (formData) => {
+  const cardData = { name: formData.title, link: formData.url };
+  const card = new Card(cardData, "#card-template", handleImageClick);
+  const cardElement = card.getView();
+  cardSection.addItem(cardElement);
+  addCardPopup.close();
+});
+addCardPopup.setEventListeners();
+
+const editProfilePopup = new PopupWithForm(
+  "#profile-edit-modal",
+  (formData) => {
+    userInfo.setUserInfo({ name: formData.title, job: formData.description });
+    editProfilePopup.close();
+  }
+);
+editProfilePopup.setEventListeners();
+
+// Initialize UserInfo
+const userInfo = new UserInfo(profileSelectors);
+
+document.querySelector("#profile-edit-button").addEventListener("click", () => {
+  const userData = userInfo.getUserInfo();
+  document.querySelector("#profile-title-input").value = userData.name;
+  document.querySelector("#profile-description-input").value = userData.job;
+  editProfilePopup.open();
+});
+
+document.querySelector("#modal-add-button").addEventListener("click", () => {
+  addCardPopup.open();
+});
+
+// Initialize Form Validation
+const profileEditFormValidator = new FormValidator(
+  validationSettings,
+  document.querySelector("#edit-profile-form")
+);
+profileEditFormValidator.enableValidation();
+
+const addCardFormValidator = new FormValidator(
+  validationSettings,
+  document.querySelector("#add-card-form")
 );
 addCardFormValidator.enableValidation();
