ddb9d57284a30c3594f5d24bbc0da5733c4b4450
James Etienne <jamespetienne@gmail.com>
2024-07-05 00:50:44 -0400

    Project 8 OOP and Webpack

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,40 +1,3 @@
-/**
-import Popup from "./Popup.js";
-
-export default class PopupWithForm extends Popup {
-  constructor(popupSelector, handleFormSubmit) {
-    super(popupSelector);
-    this._handleFormSubmit = handleFormSubmit;
-    this._form = this._popup.querySelector(".modal__form");
-    this._inputList = this._form.querySelectorAll(".modal__input");
-    this._submitButton = this._form.querySelector(".modal__submit-button");
-  }
-
-  _getInputValues() {
-    const formValues = {};
-    this._inputList.forEach((input) => {
-      formValues[input.name] = input.value;
-    });
-    return formValues;
-  }
-
-  setEventListeners() {
-    super.setEventListeners();
-    this._form.addEventListener("submit", (evt) => {
-      evt.preventDefault();
-      this._handleFormSubmit(this._getInputValues());
-      this._form.reset(); // Clear the form inputs after successful submission
-    });
-  }
-
-  close() {
-    super.close();
-    // Do not reset the form here to preserve input values if popup is closed unexpectedly
-  }
-}
-*/
-
-// New Code
 import Popup from "./Popup.js";
 
 export default class PopupWithForm extends Popup {
@@ -65,12 +28,11 @@ export default class PopupWithForm extends Popup {
     this._form.addEventListener("submit", (evt) => {
       evt.preventDefault();
       this._handleFormSubmit(this._getInputValues());
-      this._form.reset(); // Clear the form inputs after successful submission
+      this._form.reset();
     });
   }
 
   close() {
     super.close();
-    // Do not reset the form here to preserve input values if popup is closed unexpectedly
   }
 }
